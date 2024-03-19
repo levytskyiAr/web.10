d61c1d1bf31822f7c170c3ed26abc5ac17244294
Artur <arturlevuckuy@gmail.com>
2024-03-19 21:21:50 +0100

    rhm

--- web_10/quotes/views.py
+++ web_10/quotes/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-def main(request):
-    pass
+from .utils import get_mongodb
+def main(request, page = 1):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    par_page = 10
+    paginator = Paginator(list(quotes), par_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
